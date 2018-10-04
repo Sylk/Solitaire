0b74668fbd747b95c9920fec00a2a9da4393dbe2
Matthew Bauer <krakenthedestroyer@gmail.com>
2018-10-04 01:19:51 -0500

    WIP

--- src/GameModel.kt
+++ src/GameModel.kt
@@ -14,9 +14,54 @@ class GameModel {
         foundationPiles.forEach { it.reset() }
         deck.reset()
 
-        tableauPiles.forEachIndexed(i, tableauPile ->
-            val cardsInPile: MutableList<Card> - Array(i + 1, { deck.drawCard()}).toMutableList()
+        tableauPiles.forEachIndexed { i, tableauPile ->
+            val cardsInPile: MutableList<Card> = Array(i + 1, { deck.drawCard() }).toMutableList()
             tableauPiles[i] = TableauPile(cardsInPile)
-        )
+        }
+    }
+
+    fun onDeckTap() {
+        if(deck.cardsInDeck.size > 0) {
+            val card = deck.drawCard()
+            card.faceUp = true
+            wastePile.add(card)
+        } else {
+            deck.cardsInDeck = wastePile.toMutableList()
+            wastePile.clear()
+        }
+
+        fun onWasteTap() {
+            if (wastePile.size > 0 ) {
+                val card = wastePile.last()
+                if (playCard(card)) {
+                    wastePile.remove(card)
+                }
+            }
+        }
+
+        fun onFoundationTap(foundationIndex: Int) {
+            val foundationPile = foundationPiles[foundationIndex]
+
+            if (foundationPile.cards.size > 0 ) {
+                val card = foundationPile.cards.last()
+                if (playCard(card)) {
+                    foundationPile.removeCard(card)
+                }
+            }
+        }
+
+        private fun playCard(card: Card): Boolean {
+            foundationPiles.forEach {
+                if (it.addCard(card)) {
+                    return true
+                }
+            }
+            tableauPiles.forEach {
+                if (it.addCards(mutableListOf(card))) {
+                    return true
+                }
+            }
+            return false
+        }
     }
 }
